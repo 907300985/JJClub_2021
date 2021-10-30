2a0c9c3dc6c5dcf879d97a8e69598ca57612dfd1
NANDI GUO <44260801+907300985@users.noreply.github.com>
2021-10-30 10:13:25 +0800

    nandi version

--- JJProject_v2/src/cgb/jjclub/Controller.java
+++ JJProject_v2/src/cgb/jjclub/Controller.java
@@ -1,4 +1,25 @@
 package cgb.jjclub;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private Button loginBut;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField user;
+
+    @FXML
+    void login(ActionEvent event) {
+        String userName = user.getText();
+        String userpassword = password.getText();
+    }
+
 }
